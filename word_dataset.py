c952d7f5c1ae94f943ea1471dde5f4fa08b4b109
Roman Koshelev <roman.koshelev@groupdocs.com>
2017-12-17 21:37:48 +0300

    word model

--- word_dataset.py
+++ word_dataset.py
@@ -1,43 +1,80 @@
 import numpy as np
+import nltk
+import re
 
 class WordDataset:
     def __init__(self):
         self.text    = None
         self.encoded = []
     
+    
     def load(self, path):
+        STOP_WORDS   = [u"[", u"]"]
+        PUNCTS       = [u".", u",", u"?", u"!"]
+        MIN_SENT_LEN = 2
+        MAX_SENT_LEN = 30
+        
         with open(path, 'r') as f:
-            self.text=f.read()
-        self.text         = self.text.replace(u'\xa0', u' ').replace(u'\ufeff','') 
-        self.vocab        = sorted(set(self.text))
-        self.vocab_to_int = {c: i for i, c in enumerate(self.vocab)}
-        self.int_to_vocab = dict(enumerate(self.vocab))
-        self.encoded      = np.array([self.vocab_to_int[c] for c in self.text], dtype=np.int32)
+            text=f.read()
+            text = text.replace(u'\xa0', u' ').replace(u'\ufeff','')
+
+        sents = []
+        for line in text.split('\n'):
+            ls = nltk.tokenize.sent_tokenize(line)
+            for s in ls:
+                sents.extend(s.split(";"))
+
+        word_text = []
+        vocab     = set()
+        for sent in sents:
+            words = [w.lower() for w in nltk.tokenize.word_tokenize(sent) if w not in STOP_WORDS]
+            if MIN_SENT_LEN <= len(words) <= MAX_SENT_LEN:
+                if len(words) == 2 and words[1] in PUNCTS:
+                    continue
+                word_text.extend(words)
+                vocab.update(words)
+
+        self.char_text     = text
+        self.word_text     = word_text
+        self.word_to_token = {w: i for i, w in enumerate(vocab)}
+        self.token_to_word = dict(enumerate(vocab))
+        self.num_tokens    = len(vocab)
+        self.encoded       = self.encode(word_text)
+
+
+    def encode(self, words):
+        return np.array([self.word_to_token[w] for w in words], dtype=np.int32)
+
+    
+    def decode(self, tokens):
+        text = " ".join([self.token_to_word[t] for t in tokens])
+        return text
+
+    
+    def decode_ext(self, tokens):
+        text = " ".join([self.token_to_word[t] for t in tokens])
+        text = text.replace(' .', '.').replace(' ,', ',')
+        snts = text.split('.')
+        text = ''
+        for s in snts:
+            s = s.strip()
+            if len(s)<1: 
+                continue
+            s = s[0].upper() + s[1:]+'. '
+            text += s
+        return text.strip()
+
     
     def get_batches(self, n_seqs, n_steps):
-        '''Создаем генератор, который возвращает пакеты размером
-           n_seqs x n_steps из массива arr.
-
-           Аргументы
-           ---------
-           n_seqs: Batch size, количество последовательностей в пакете
-           n_steps: Sequence length, сколько "шагов" делаем в пакете
-        '''
         arr = self.encoded
-        # Считаем количество символов на пакет и количество пакетов, которое можем сформировать
-        characters_per_batch = n_seqs * n_steps
-        n_batches = len(arr)//characters_per_batch
+        words_per_batch = n_seqs * n_steps
+        n_batches = len(arr)//words_per_batch
 
-        # Сохраняем в массиве только символы, которые позволяют сформировать целое число пакетов
-        arr = arr[:n_batches * characters_per_batch]
-
-        # Делаем reshape 1D -> 2D, используя n_seqs как число строк, как на картинке
+        arr = arr[:n_batches * words_per_batch]
         arr = arr.reshape((n_seqs, -1))
 
         for n in range(0, arr.shape[1], n_steps):
-            # пакет данных, который будет подаваться на вход сети
             x = arr[:, n:n+n_steps]
-            # целевой пакет, с которым будем сравнивать предсказание, получаем сдвиганием "x" на один символ вперед
             y = np.zeros_like(x)
             y[:, :-1], y[:, -1] = x[:, 1:], x[:, 0]
             yield x, y
---
+++ word_model.py
@@ -0,0 +1,159 @@
+import os
+from utils import make_dir
+import numpy as np
+import tensorflow as tf
+import pickle
+from visualization import show_train_stats
+
+class WordRNN:
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        # state
+        self.tr_step   = 0
+        self.tr_epoch  = 0
+        self.tr_losses = []
+        
+    
+    def _make_rnn_cell(self, num_units, num_layers, keep_prob):
+        def make_layer():
+            l = tf.contrib.rnn.BasicLSTMCell(num_units)
+            l = tf.contrib.rnn.DropoutWrapper(l, output_keep_prob=keep_prob)
+            return l        
+        layers = [make_layer() for _ in range(num_layers)]
+        cell   = tf.contrib.rnn.MultiRNNCell(layers)
+        return cell
+
+    
+    def _make_loss(self, logits, targets, lstm_size, num_classes):
+        loss = tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits, 
+            labels=tf.one_hot(targets, num_classes))
+        loss = tf.reduce_mean(loss)
+        return loss
+    
+
+    def _make_optimizer(self, loss, lr, grad_clip):
+        tr_vars   = tf.trainable_variables()
+        grads, _  = tf.clip_by_global_norm(tf.gradients(loss, tr_vars), grad_clip)
+        train_op  = tf.train.AdamOptimizer(lr)
+        optimizer = train_op.apply_gradients(zip(grads, tr_vars))
+        return optimizer
+    
+    
+    def _get_initial_state(self, batch_size):
+        return self._sess.run(self.initial_state, feed_dict={self.batch_size_pl: batch_size})
+
+    
+    def build(self, num_units, num_layers, grad_clip):
+        tf.reset_default_graph()
+        self._graph = tf.Graph()
+        self._scope  = "char_rnn"
+        with self._graph.as_default(), tf.variable_scope(self._scope):
+            # placeholders
+            self.inputs_pl     = tf.placeholder(tf.int32, [None, None], name='inputs')
+            self.targets_pl    = tf.placeholder(tf.int32, [None, None], name='targets')
+            self.seq_length_pl = tf.placeholder(tf.int32, [None], 'seq_lengths')
+            self.batch_size_pl = tf.placeholder(tf.int32, shape=[], name='batch_size')
+            self.keep_prob_pl  = tf.placeholder(tf.float32, name='keep_prob')
+            self.lr_pl         = tf.placeholder(tf.float32, name='learning_rate')
+
+            # network
+            embed              = tf.one_hot(self.inputs_pl, self.num_classes)
+            cell               = self._make_rnn_cell(num_units, num_layers, self.keep_prob_pl)
+            initial_state      = cell.zero_state(self.batch_size_pl, tf.float32)
+            outputs, state     = tf.nn.dynamic_rnn(cell, embed, self.seq_length_pl, initial_state, dtype=tf.float32)
+            self.initial_state = initial_state
+            self.final_state   = state
+
+            # prediction
+            logits             = tf.layers.dense(outputs, self.num_classes)
+            self.prediction    = tf.nn.softmax(logits, name='predictions')
+
+            # training
+            self.loss_op       = self._make_loss(logits, self.targets_pl, num_units, self.num_classes)
+            self.train_op      = self._make_optimizer(self.loss_op, self.lr_pl, grad_clip)
+            
+            # utils
+            self.init_op       = tf.global_variables_initializer()
+            self._saver        = tf.train.Saver()
+
+        # session
+        self._sess = tf.Session(graph=self._graph)
+        self._sess.run(self.init_op)
+        
+        
+    def train(self, dataset, seq_length, epochs, batch_size, keep_prob, learning_rate, log_every=10, mean_win=10):
+        try:
+            for self.tr_epoch in range(self.tr_epoch, epochs):
+                state = self._get_initial_state(batch_size)
+
+                for x, y in dataset.get_batches(batch_size, seq_length):
+                    self.tr_step += 1
+                    tr_loss, state, _ = self._sess.run(
+                        [self.loss_op, self.final_state, self.train_op], 
+                        feed_dict = {
+                            self.inputs_pl    : x,
+                            self.targets_pl   : y,
+                            self.seq_length_pl: [seq_length, ]*batch_size,
+                            self.initial_state: state,
+                            self.keep_prob_pl : keep_prob,
+                            self.lr_pl        : learning_rate,
+                    })
+                    self.tr_losses.append(tr_loss)
+                    
+                    if self.tr_step % log_every == 0:
+                        show_train_stats(self.tr_epoch, self.tr_step, self.tr_losses, mean_win)
+                        
+        except KeyboardInterrupt:
+            show_train_stats(self.tr_epoch, self.tr_step, self.tr_losses, mean_win)
+            
+
+    def save(self, path):
+        make_dir(path)
+        pickle.dump([self.tr_epoch, self.tr_step, self.tr_losses], open(os.path.join(path, "state.p"), "wb"))
+        self._saver.save(self._sess, path)
+        
+    def restore(self, path):
+        try:
+             [self.tr_epoch, self.tr_step, self.tr_losses] = pickle.load(open(os.path.join(path, "state.p"), "rb"))
+        except: 
+            print("State not found at", path)
+        self._saver.restore(self._sess, path)
+        
+    def predict(self, x, state, seq_len):
+        pred, state = self._sess.run(
+            [self.prediction, self.final_state], 
+            feed_dict={
+                self.inputs_pl    : x,
+                self.seq_length_pl: [seq_len],
+                self.initial_state: state,
+                self.keep_prob_pl : 1.
+            })
+        return pred, state
+    
+    def sample(self, dataset, n_samples, top_n, prime):
+        seq_len = 1
+        def pick_top_n(pred, vocab_size, top_n):
+            p = np.squeeze(pred)
+            p[np.argsort(p)[:-top_n]] = 0
+            p = p / np.sum(p)
+            c = np.random.choice(vocab_size, 1, p=p)[0]
+            return c
+
+        samples = [c for c in prime]
+        state = self._get_initial_state(batch_size = 1)
+
+        for c in prime:
+            x      = np.zeros([1, seq_len])
+            x[0,0] = dataset.vocab_to_int[c]
+            preds, state = self.predict(x, state, seq_len)
+        c = pick_top_n(preds, self.num_classes, top_n)
+        samples.append(dataset.int_to_vocab[c])
+
+        for i in range(n_samples):
+            x[0,0] = c
+            preds, state = self.predict(x, state, seq_len)
+            c = pick_top_n(preds, self.num_classes, top_n)
+            samples.append(dataset.int_to_vocab[c])
+
+        return ''.join(samples)
