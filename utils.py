ca64fdcefc16a770c945016e73dc3ee24f7be38a
Roman Koshelev <roman.koshelev@groupdocs.com>
2018-01-21 16:10:21 +0000

    dataset refactored

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import shutil
+import nltk
+
 
 def make_dir(path: str):
     folder = os.path.dirname(path)
@@ -8,12 +10,30 @@ def make_dir(path: str):
         os.makedirs(folder)
     return path
 
+
 def remove_dir(path: str):
     if os.path.exists(path):
         shutil.rmtree(path)
 
+        
 def prepare_sample(s):
     s = s.replace(' .', '.').replace(' ,', ',').replace(' !', '!').replace(' ?', '?').replace(' :', ':')
     s = s.replace('— —', '—').replace(',,', ',').replace(',.', ',').replace('« ', '«').replace(' »', '»')
     s = s.replace('—,', '—')
+    return s
+
+
+def sent_to_words(sent):
+    IGNORE = "[]{}<>~@#$%^/\|_+*…«»\"\'" #()–:
+    s = sent.lower()
+    for c in IGNORE:
+        s=s.replace(c, ' ')
+    return [w for w in nltk.tokenize.word_tokenize(s)]
+
+
+def words_to_sent(words):
+    s = ' '.join(words)
+    s = s.replace(' ,', ',').replace(' !', '!').replace(' ?', '?').replace(' .', '.').replace(' :', ':')
+    s = s.replace(' )', ')').replace('( ', '(').replace(' ;', ';')
+    s = s[0].upper() + s[1:]
     return s
---
+++ vocabulary.py
@@ -0,0 +1,59 @@
+import numpy as np
+import nltk
+from nltk import FreqDist
+import pickle
+
+UNK, UNK_CODE = '<UNK>', 0
+BOS, BOS_CODE = '<BOS>', 1
+EOS, EOS_CODE = '<EOS>', 2
+PAD, PAD_CODE = '<PAD>', 3
+
+class Vocabulary:
+    def __init__(self):
+        self._tokens_to_words = None
+        self._words_to_tokens = None
+    
+    @staticmethod
+    def _update(fdist, path):
+        with open(path, 'r') as f:
+            text = f.read()
+            text = text.replace('\xa0', ' ').replace('\ufeff','')
+            text = text.lower()
+
+        for sentence in nltk.tokenize.sent_tokenize(text):
+            for word in nltk.tokenize.word_tokenize(sentence):
+                fdist[word]+=1
+        return fdist               
+
+    @property
+    def size(self):
+        return len(self._tokens_to_words)
+    
+    def build(self, paths, max_size=30000):
+        if type(paths) is str:
+            paths = [paths]
+
+        fdist = FreqDist()
+        for p in paths:
+            fdist = self._update(fdist, p)
+            
+        most_common = fdist.most_common(max_size)
+        words       = [ UNK, BOS, EOS, PAD ] + [w for w, _ in most_common]
+        self._tokens_to_words = words
+        self._words_to_tokens = {words[i]:i for i in range(len(words))}        
+
+        
+    def save(self, path):
+        pickle.dump([self._tokens_to_words, self._words_to_tokens], open(path, "wb"))
+
+        
+    def restore(self, path):
+        [self._tokens_to_words, self._words_to_tokens] = pickle.load(open(path, "rb"))
+        
+        
+    def to_tokens(self, words):
+        return [self._words_to_tokens.get(w, UNK_CODE) for w in words]
+
+    
+    def to_words(self, tokens):
+        return [self._tokens_to_words[t] for t in tokens]
